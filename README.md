4f9c83fa70cbc5f72e4eb78243ce7e7f93ea5ea9
Shijun Qin <272700767@qq.com>
2016-12-01 15:05:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# RedHat_Inc_Assignment
+
+This assignment is an exam for Java developer internship in [RedHat Inc](https://www.redhat.com/en).The concrete contents are as follows:
+
+> Create a maven project, The output of this project is a war which can be deployed to tomcat/jetty/jboss. The war/web app can respond “hello world “ with servlet for url “http://localhost:8080/test/hello”. It’s better you can add a test for this servlet. push it to github repo, Create a README to describe how to build and run your project in English.
+
+So, firstly you need to prepare your development environment. My development environment is as follows:
+
+* OS X 10.11.6
+* Oracle JDK 1.8.0_65
+* Apache Maven 3.3.9
+* Apache Tomcat 8.0.32
---
+++ src/main/java/org/lioncruise/test/HelloWorldServlet.java
@@ -0,0 +1,40 @@
+package org.lioncruise.test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorldServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("hello world");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
